9d8b617f06edfc3f437356ab6a6ce928f846d895
arisnance <nance.aris@gmail.com>
2017-08-17 00:11:03 +0000

    SMSSupdate

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -10,7 +10,7 @@ class Subscription < ActiveRecord::Base
     @twilio_number = '+12065391879'
 		@client = Twilio::REST::Client.new 'AC4e9ac55e2bbd7fc3a434e3d37896782e','5934a63e2b41eddfaa0275f20b328db9'
     time_str = ((self.time).localtime).strftime("%I:%M%p on %b. %d, %Y")
-    reminder = "Hi Just a reminder that your #{self.title} subscription should be canceled on #{time_str}."
+    reminder = "Hi Just a reminder that you should cancel your #{self.title} subscription on #{time_str}."
     @client.messages.create(
       :from => @twilio_number,
       :to => self.phone_number,
